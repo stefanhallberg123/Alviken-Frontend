bc9b97d36433b4c2df6e745cdd22e75dca1903d9
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-08-27 15:09:11 +0200

    post-function is working

--- src/components/formModal/FormModal.tsx
+++ src/components/formModal/FormModal.tsx
@@ -1,19 +1,68 @@
-import React from 'react'
-import "./formModal.scss"
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+import "./formModal.scss";
 
 export default function Formmodal() {
-    return (
-        <div >
-            <form className="userForm">
 
-                <h2>Boka Bord</h2>
-                <input name="name" placeholder="Namn" type="text" />
-                <input name="email" placeholder="Email" type="text" />
-                <input name="phone" placeholder="Telefonnummer" type="text" />
-                <input name="comment" placeholder="Meddelande till resturangen" type="text" />
-                <button type="button">BOKA</button>
+  const [bookingUser, setbookingUser] = useState({
+    name: "", phone: "", comment: "", email: ""
+  });
 
-            </form>
-        </div>
-    )
+  const handleData = (e: any) => {
+    setbookingUser({ ...bookingUser, [e.target.name]: e.target.value })
+  };
+
+
+  const sendData = (event: any) => {
+    event.preventDefault();
+  }
+  useEffect(() => {
+    axios.post('http://localhost:5000/', bookingUser).then(
+      respone => {
+        console.log(respone.data);
+      }).catch(function (err) {
+        console.log(err);
+      })
+  }, []);
+
+  return (
+    <div className="formDiv">
+      <form onSubmit={sendData}>
+        <h1>Boka Bord</h1>
+        <input className="name"
+          name="name"
+          placeholder="Namn"
+          type="text"
+          value={bookingUser.name}
+          onChange={handleData}
+          required />
+        <input
+          className="email"
+          name="email"
+          placeholder="Email"
+          type="email"
+          value={bookingUser.email}
+          onChange={handleData}
+          required
+        />
+        <input
+          className="phone"
+          name="phone"
+          placeholder="Telefonnummer"
+          type="number"
+          value={bookingUser.phone}
+          onChange={handleData}
+          required
+        />
+        <textarea
+          className="comment"
+          name="comment"
+          placeholder="Meddelande till resturangen"
+          value={bookingUser.comment}
+          onChange={handleData}
+        />
+        <button id="normalbutton" type="submit">BOKA</button>
+      </form>
+    </div>
+  );
 }
