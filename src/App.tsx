3269615f40abb499c063ecb89ebaad5b8289a672
OSKAR-exe <oskar.aguado.nilsson@gmail.com>
2020-08-28 12:43:37 +0200

    landingpage early structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Formmodal from './components/formModal/FormModal';
+import LandingPage from './components/landingpage/landingpage';
 
 function App() {
   return (
     <div className="App">
-      <Formmodal></Formmodal>
+      {/* <Formmodal></Formmodal> */}
+      <LandingPage></LandingPage>
     </div>
   );
 }
--- src/components/landingpage/landingpage.tsx
+++ src/components/landingpage/landingpage.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import "./landingpage.scss"
 
 export default function LandingPage() {
 
     return (<div>
+        <div className="landingpage-body">
+        <section className="hero">
+            <div className="hero-inner">
+                <h1>Alviken</h1>
+                <h2>Välkommen att boka bord hos oss!</h2>
+                <div className="booking">
+                    <div className="day">lorem ipsum</div>
+                    <div className="time">lorem ipsum</div>
+                    <div className="people">lorem ipsum</div>
+                    <div className="bookbutton">Boka</div>
+                </div>
+                
+            </div>
+        </section>
+        </div>
     </div>)
 
 
