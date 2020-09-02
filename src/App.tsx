b30666fec40fa772a06120814cf7dc2806cb64bb
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-09-02 13:17:39 +0200

    fixed some errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,41 @@ import React from "react";
 import "./App.scss";
 import Modal from "./components/modal/Modal/Modal";
 import LandingPage from "./components/landingpage/landingpage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ThankYou from "./components/thankyou/thankyou";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage></LandingPage>
+      <Router>
+        <Switch>
+          <Route path="/thankyou">
+            <ThankYou></ThankYou>
+          </Route>
+          <Route path="/modal">
+            <Modal></Modal>
+          </Route>
+          <Route path="/">
+            <LandingPage></LandingPage>
+          </Route>
+
+          {/* <Route path="*">
+            <NoMatch></NoMatch>
+          </Route> */}
+          {/* <Route path="/admin">
+            <Admin></Admin>
+          </Route> */}
+          {/* <Route path="/admin">
+            <Home></Home>
+          </Route> */}
+          {/* <Route path="/admin/create">
+            <Create></Create>
+          </Route> */}
+          {/* <Route path="/admin/edit">
+            <Edit></Edit>
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/landingpage/landingpage.tsx
+++ src/components/landingpage/landingpage.tsx
@@ -1,79 +1,76 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "./landingpage.scss";
-import DayPickerInput from 'react-day-picker/DayPickerInput';
-import 'react-day-picker/lib/style.css';
-import Axios from 'axios';
-import { runInThisContext } from 'vm';
+import Axios from "axios";
+import Reactmodal from "react-modal";
 
 export default function LandingPage() {
-    const [selectedDate, setSelectedDate] = useState("");
-    const [selectedTime, setSelectedTime] = useState(null);
-    const [selectedPeople, setSelectedPeople] = useState(null);
-    function checkAvailability(){
-        console.log(selectedDate)
-        console.log(selectedTime)
-        console.log(selectedPeople)
+  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [selectedPeople, setSelectedPeople] = useState(null);
+  // modal
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
 
-        Axios.get("http://localhost:5000/?date="+selectedDate+"&time="+selectedTime+"&people="+selectedPeople).then(response => response.data()).then(bookings =>{
-            console.log(bookings)
-        })
-    }
+  function checkAvailability() {
+    console.log(selectedDate);
+    console.log(selectedTime);
+    console.log(selectedPeople);
 
-    return (<div>
-        <div className="landingpage-body">
+    Axios.get(
+      "http://localhost:5000/?date=" +
+        selectedDate +
+        "&time=" +
+        selectedTime +
+        "&people=" +
+        selectedPeople
+    )
+      .then((response) => response.data())
+      .then((bookings) => {
+        console.log(bookings);
+      });
+    setIsOpen(true);
+  }
+
+  return (
+    <div>
+      <div className="landingpage-body">
         <section className="hero">
-            <div className="hero-inner">
-                <h1>Alviken</h1>
-                <h2>Välkommen att boka bord hos oss!</h2>
-                <div className="booking">
-                    <div className="day">
-                    <DayPickerInput onDayChange={day => {
-                        console.log(day.toLocaleDateString())
-                        setSelectedDate(day.toLocaleDateString());
-                        }}/> 
-                        </div> 
-                    <div className="time">
-                    <form>
+          <div className="hero-inner">
+            <h1>Alviken</h1>
+            <h2>Välkommen att boka bord hos oss!</h2>
+            <div className="booking">
+              <div className="day">
+                {/* <DayPickerInput onDayChange={day => { */}
+                console.log(day.toLocaleDateString())
+                setSelectedDate(day.toLocaleDateString());
+                {/* }}/>  */}
+              </div>
+              <div className="time">
+                <form>
+                  <div>
+                    <label>
+                      <input type="radio" value="18" checked={true} />
+                      18:00
+                    </label>
+                    <label>
+                      <input type="radio" value="21" />
+                      21:00
+                    </label>
+                  </div>
 
-                <div>
-                <label>
-                    <input
-                    type="radio"
-                    value="18"
-                    checked={true}
-                
-                    
-                    />
-                    18:00
-                </label>
-                <label>
-                    <input
-                    type="radio"
-                    value="21"
-                
-                
-                    />
-                    21:00
-                </label>
-                </div>
-
-                <button type="button" 
-                onClick={checkAvailability}
-                >
+                  <button type="button" onClick={checkAvailability}>
                     Boka
-                </button>
-
+                  </button>
                 </form>
-            </div> 
-                    <div className="people">lorem ipsum</div> 
-                    <div className="bookbutton">
-                    </div>
-                </div>
-                
+              </div>
+              <div className="people">lorem ipsum</div>
+              <div className="bookbutton"></div>
             </div>
+          </div>
         </section>
-        </div>
-    </div>)
-
-
+      </div>
+    </div>
+  );
 }
