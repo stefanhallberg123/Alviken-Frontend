a3fbff6192f523281e2a773e01299ee65b5f2d18
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-09-09 15:23:43 +0200

    fixed edit calendar

--- src/components/landingpage/modal/Modal/Modal.tsx
+++ src/components/landingpage/modal/Modal/Modal.tsx
@@ -2,15 +2,14 @@ import React, { useState } from "react";
 import ModalForm, { IUserValue } from "../ModalForm/ModalForm";
 import axios from "axios";
 import "./Modal.scss";
-import LandingPage from "../../landingpage";
 
 export default function Modal() {
   // hämtar datan från ModalForm och skickan in det i setUserValue
 
   const updateUser = (people: any) => {
-    setuserValue({people})
+    setuserValue({ people });
     console.log(people);
-  }
+  };
   const [, setuserValue] = useState({});
   const updateValue = (bookingUser: IUserValue) => {
     setuserValue({ bookingUser });
